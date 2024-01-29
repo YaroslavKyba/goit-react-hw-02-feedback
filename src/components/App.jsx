513dfb0486cd8457c83fefefb78cc1a809481fe6
YaroslavKyba <narxoc.ne@gmail.com>
2024-01-29 12:45:33 +0200

    1 step

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,33 @@
-export const App = () => {
-  return <div>React homework template</div>;
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleClick = () => {
+    this.setState(prevState => ({ good: prevState.good + 1 }));
+  };
+
+  render() {
+    const keys = Object.keys(this.state);
+
+    return (
+      <div>
+        <p>Please leave feedback</p>
+        {keys?.map(key => {
+          return (
+            <button onClick={this.handleClick}>{key.toUpperCase()}</button>
+          );
+        })}
+        <p>Statistics</p>
+        <p>
+          Good:{this.state.good} Neutral: {this.state.neutral} Bad:
+          {this.state.bad}
+        </p>
+      </div>
+    );
+  }
+}
